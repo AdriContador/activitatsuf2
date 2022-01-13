e4f12ead3bd92809cd85e091d007283d74d9cb7b
AdriContador <95416860+AdriContador@users.noreply.github.com>
2022-01-13 13:04:29 +0100

    Add files via upload

---
+++ ex1/functions.c
@@ -0,0 +1,22 @@
+/*
+ * functions.c
+ *
+ *  Created on: 12 ene 2022
+ *      Author: adricontador
+ */
+
+int validate(){
+	int num, i=0;
+		do{
+			printf("Escriu un nombre entre 10 i 5000:");
+			scanf("%d", &num);
+			i++;
+		}while (num>5000 || num<10 && i<3);
+		if (num<10 || num>5000){
+			printf("El número introduït no és vàlid.");
+		}
+		else{
+		return num;
+		}
+}
+
---
+++ ex1/main.c
@@ -0,0 +1,14 @@
+/*
+ * main.c
+ *
+ *  Created on: 12 ene 2022
+ *      Author: adricontador
+ */
+
+#include <stdio.h>
+#include "functions.h"
+void main(){
+	int num1;
+	num1 = validate();
+
+}
