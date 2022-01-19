0494cd0960d2cd3a3fb5064c7702ead88f9f0c51
AdriContador <95416860+AdriContador@users.noreply.github.com>
2022-01-19 14:15:38 +0100

    Add files via upload

---
+++ 2uf2/main.c
@@ -0,0 +1,14 @@
+/*
+ * main.c
+ *
+ *  Created on: 12 ene 2022
+ *      Author: adricontador
+ */
+
+#include <stdio.h>
+#include "functions.h"
+void main(){
+	int segons;
+	segons = introduir();
+	time(segons);
+}
