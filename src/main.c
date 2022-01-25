e6ae96869edae00d75aff211a5946183499fc008
AdriContador <95416860+AdriContador@users.noreply.github.com>
2022-01-25 10:18:23 +0100

    Add files via upload

---
+++ src/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ *  Created on: 21 ene 2022
+ *      Author: adricontador
+ */
+
+#include <stdio.h>
+#include "functions.h"
+
+void main(){
+	int num1, num2;
+	num1 = value();
+	num2 = value();
+	change(num1, num2);
+}
