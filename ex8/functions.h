a23a39a2a4138d3556b424d3a9d2a1d65adb6eb9
AdriContador <95416860+AdriContador@users.noreply.github.com>
2022-01-25 10:54:03 +0100

    Add files via upload

---
+++ ex8/functions.h
@@ -0,0 +1,17 @@
+/*
+ * functions.h
+ *
+ *  Created on: 25 ene 2022
+ *      Author: adricontador
+ */
+
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+
+
+
+#endif /* FUNCTIONS_H_ */
+
+int value();
+int value2();
+void elevate(num1, num2);
---
+++ ex8/main.c
@@ -0,0 +1,17 @@
+/*
+ * main.c
+ *
+ *  Created on: 25 ene 2022
+ *      Author: adricontador
+ */
+
+#include <stdio.h>
+#include "functions.h"
+
+void main(){
+	int num1, num2;
+	num1 = value();
+	num2 = value2();
+	elevate(num1, num2);
+}
+
